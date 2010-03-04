8948df429a81c61c4a673d1cf1e3b993d53790d7
Fabian Schmied <fabian.schmied@rubicon.eu>
2010-03-04 12:37:59 +0000

    Added TypeDiscovery sample

---
+++ TypeDiscovery/Program.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2009 rubicon informationstechnologie gmbh
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using Remotion.Logging;
+using Remotion.Reflection.TypeDiscovery;
+using System.Linq;
+using Remotion.Text;
+
+namespace TypeDiscovery
+{
+  public class Program
+  {
+    private static void Main ()
+    {
+      LogManager.Initialize ();
+
+      var typeDiscoveryService = ContextAwareTypeDiscoveryUtility.GetTypeDiscoveryService ();
+      Console.WriteLine ("Type discovery service type: {0}", typeDiscoveryService.GetType ());
+      Console.WriteLine ();
+
+      var types = typeDiscoveryService.GetTypes (null, false);
+      var assemblies = types.Cast<Type> ().Select (t => t.Assembly).Distinct ().ToArray ();
+      Console.WriteLine ("Found {0} types from {1} assemblies", types.Count, assemblies.Length);
+      Console.WriteLine ();
+
+      Console.WriteLine ("Assemblies:");
+      Console.WriteLine (SeparatedStringBuilder.Build (", ", assemblies, a => a.GetName ().Name));
+      Console.WriteLine ();
+
+      Console.WriteLine ("Types (first 100): ");
+      Console.WriteLine (SeparatedStringBuilder.Build (", ", types.Cast<Type> ().Take (100), t => t.Name));
+    }
+  }
+}
---
+++ TypeDiscovery/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("TypeDiscovery")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Microsoft")]
+[assembly: AssemblyProduct ("TypeDiscovery")]
+[assembly: AssemblyCopyright ("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible (false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid ("99b81d72-9175-4cb8-8ca2-39e39cdf4086")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
